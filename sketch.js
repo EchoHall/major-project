6a39ccc7ffcb1d22a28828207d8b39c78b372875
EchoHall <lehoangquan.lhq@gmail.com>
2024-11-22 11:02:53 -0600

    testing and experimenting with physics engine

--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
 // Major Project
+// p5Play engine
 
+
+let box, floor;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  new Canvas(windowWidth, windowHeight);
+
+  box = new Sprite();
+  box.x = 200;
+  box.width = 50;
+  box.height = 50;
+  
+  floor.width = width;
+  floor.y = 50;
+  
+
 }
 
 function draw() {
-  background(220);
-  square(100, 100, 50);
+  clear();
 }
