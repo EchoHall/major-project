11c1d71dd3ae3fa5ee98eeb8f98adc8b36f14ee1
EchoHall <lehoangquan.lhq@gmail.com>
2024-11-29 10:05:58 -0600

    trying to make road move

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 // p5Play engine
 
 
-let car;
+let road, car;
 
 function setup() {
   new Canvas(windowWidth, windowHeight);
@@ -12,10 +12,21 @@ function setup() {
   car.x = windowWidth/2;
   car.w = 50;
   car.h = 100;
+  car.layer = 2;
 
+  road = new Sprite();
+  road.y = windowHeight/2;
+  road.x = windowWidth/2;
+  road.w = 400;
+  road.h = windowHeight;
+  road.color = "black";
+  road.layer = 1;
 }
 
 function draw() {
   clear();
   car.rotateTowards(mouse, 0.1, 90);
+  car.overlaps(road);
+
+  
 }
